5d076201db820d13dee1f7731df7f5bd4384a9a0
Dyego Maas <dyego.maas@gmail.com>
2019-03-12 23:49:58 -0300

    Creating an overly complicated server application.

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PG_USER,
+    pgHost: process.env.PG_HOST,
+    PgDatabase: process.env.PG_DATABASE,
+    PgPassword: process.env.PG_PASSWORD,
+    PgPort: process.env.PG_PORT
+}
