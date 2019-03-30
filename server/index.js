98c73e4b0948eb1d9419de4082e4457a2b219597
Dyego Maas <dyego.maas@gmail.com>
2019-03-29 23:38:43 -0300

    Last trial today.

--- server/index.js
+++ server/index.js
@@ -34,22 +34,22 @@ const redisClient = redis.createClient({
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send('Hi');
 })
 
-app.get('/values/all', async (req, res) => {
+app.get('/api/values/all', async (req, res) => {
     const values = await pgClient.query('SELECT * FROM Values');
     res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/api/values/current', async (req, res) => {
     redisClient.hgetall('values', (err, values) => {
         res.send(values);
     });
 });
 
-app.post('/values', async (req, res) => {
+app.post('/api/values', async (req, res) => {
     const { index } = req.body;
 
     if (parseInt(index) > 40) {
